59e509cecf6c2f79495c1f19bdaeef7a3345bc44
kikiade <adwtyaskiki@gmail.com>
2017-02-13 16:31:23 +0700

    benerin tampilan dashboar

--- app/webroot/js/demo_dashboard.js
+++ app/webroot/js/demo_dashboard.js
@@ -27,55 +27,11 @@ $(function(){
     /* end reportrange */
     
     /* Rickshaw dashboard chart */
-    var seriesData = [ [], [] ];
-    var random = new Rickshaw.Fixtures.RandomData(1000);
-
-    for(var i = 0; i < 100; i++) {
-        random.addData(seriesData);
-    }
-
-    var rdc = new Rickshaw.Graph( {
-            element: document.getElementById("dashboard-chart"),
-            renderer: 'area',
-            width: $("#dashboard-chart").width(),
-            height: 250,
-            series: [{color: "#33414E",data: seriesData[0],name: 'New'}, 
-                     {color: "#3FBAE4",data: seriesData[1],name: 'Returned'}]
-    } );
-
-    rdc.render();
-
-    var legend = new Rickshaw.Graph.Legend({graph: rdc, element: document.getElementById('dashboard-legend')});
-    var shelving = new Rickshaw.Graph.Behavior.Series.Toggle({graph: rdc,legend: legend});
-    var order = new Rickshaw.Graph.Behavior.Series.Order({graph: rdc,legend: legend});
-    var highlight = new Rickshaw.Graph.Behavior.Series.Highlight( {graph: rdc,legend: legend} );        
-
-    var rdc_resize = function() {                
-            rdc.configure({
-                    width: $("#dashboard-chart").width(),
-                    height: $("#dashboard-chart").height()
-            });
-            rdc.render();
-    }
-
-    var hoverDetail = new Rickshaw.Graph.HoverDetail({graph: rdc});
-
-    window.addEventListener('resize', rdc_resize);        
-
-    rdc_resize();
+    
     /* END Rickshaw dashboard chart */
     
     /* Donut dashboard chart */
-    Morris.Donut({
-        element: 'dashboard-donut-1',
-        data: [
-            {label: "Returned", value: 2513},
-            {label: "New", value: 764},
-            {label: "Registred", value: 311}
-        ],
-        colors: ['#33414E', '#3FBAE4', '#FEA223'],
-        resize: true
-    });
+    
     /* END Donut dashboard chart */
     
     /* Bar dashboard chart */
